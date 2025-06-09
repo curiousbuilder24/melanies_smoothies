b7d5207cd5ee4633e33699ba29a598362e619d44
curiousbuilder24 <kchande1@outlook.com>
2025-06-09 15:37:15 -0400

    Update streamlit_app.py
    added space between comma before col() for SEARCH_ON

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ session = cnx.session()
 
 #Display the Fruit Options List loaded from STAGE in SiS App
 #Add the New SEARCH_ON Column to the Dataframe that feeds the Multiselect
-my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_Name'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_Name') ,col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
